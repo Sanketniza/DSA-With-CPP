1e656a6de52e9af99d91ef119d9902d0294a669d
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-22 19:56:43 +0530

    adding same recursion problem for different types of objects with different properties and properties in the same order

---
+++ C++ Question/Lecture 31 32 33 34 35 36 37 38 = Recursion 1/Question's/38_Phone_KeyPad.cpp
@@ -0,0 +1,84 @@
+/* 
+17. Letter Combinations of a Phone Number
+Solved
+Medium
+Topics
+Companies
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(string str, vector<string> &ans, string output, int index, string keyPad[])
+{
+
+    // base case
+
+    if (index >= str.length())
+    {
+        ans.push_back(output);
+        return;
+    }
+
+    int number = str[index] - '0';
+    string value = keyPad[number];
+
+    for (int i = 0; i < value.length(); i++)
+    {
+        output.push_back(value[i]);
+        solve(str, ans, output, index + 1, keyPad);
+        output.pop_back();
+    }
+}
+
+int main()
+{
+
+    string str;
+    cout << "Enter the string : ";
+    cin >> str;
+
+    vector<string> ans;
+
+    if (str.length() == 0)
+    {
+        ans.push_back("");
+        return 0;
+    }
+
+    string output = "";
+    int index = 0;
+
+    string keyPad[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    solve(str, ans, output, index, keyPad);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
