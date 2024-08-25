af78c28764cf06eeb955bbd7489c269074e0886d
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-26 00:04:23 +0530

    (Modified C++ examples in Lecture 31-38 to enhance understanding of recursion)

--- C++ Question/Lecture 31 32 33 34 35 36 37 38 = Recursion 1/Question's/6_Print_No_ToWord.cpp
+++ C++ Question/Lecture 31 32 33 34 35 36 37 38 = Recursion 1/Question's/6_Print_No_ToWord.cpp
@@ -8,16 +8,23 @@ void sayDigit(int n, string arr[])
 
     // base case
 
-    if (n == 0)
-    {
+    if(n < 0) {
         return;
     }
 
+    if (n == 0)
+    {
+        cout <<  arr[0];
+        return;
+    } 
+
     // processing
     int digit = n % 10;
+    cout << arr[digit] << endl;
     n = n / 10;
+    cout << n << endl;
 
-// cout << arr[digit] << " " << endl; --> print number in reverse order
+   // cout << arr[digit] << " " << endl; --> print number in reverse order
 
     // recursive case
     sayDigit(n, arr);
