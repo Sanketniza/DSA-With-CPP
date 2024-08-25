96572fce3e16516f9e1794474afd0a23c3d01169
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-08-25 12:34:19 +0530

    (Modified C++ examples in Lecture 26 to enhance understanding of pointers)

--- C++ Question/Lecture 26 = Pointer in C++/1_Array_Address.cpp
+++ C++ Question/Lecture 26 = Pointer in C++/1_Array_Address.cpp
@@ -5,18 +5,22 @@ using namespace std;
 
 void print(int *a)
 {
-    cout << "inside the function " << a << endl;
-    cout << "inside the function " << *a << endl;
-    cout << "inside the function " << &a << endl;
+    cout << "1 inside the function " << a << endl;  // address of int a = 10 
+    cout << "2 inside the function " << *a << endl; // print value of int a = 10
+    cout << "3 inside the function " << &a << endl << endl; // print address of *p = *a
 }
 
 void update(int *a)
 {
-    // a = a + 1;
-    // cout << "inside the function of update " << a << endl;
-    // cout << "inside the function of update " << &a << endl;
+    a = a + 1;
+    cout << "5 inside the function of update " << a << endl;
+    cout << "6 inside the function of update " << &a << endl;
 
     *a = *a + 1;
+    cout << "7 inside the function of update " << a << endl;
+    cout << "8 inside the function of update " << &*a << endl;
+    cout << "9 inside the function of update " << *a << endl << endl;
+
 }
 
 int sum(int *arr , int n)
@@ -31,12 +35,12 @@ int sum(int *arr , int n)
 
 int main()
 {
-    /*
+    /* 
         int arr[]= {8,2,3,4,5};
 
         cout<<arr<<endl;
         cout << "The address is first index is : " << arr << endl;
-        cout << "The address is second index is : " << &arr[0] << endl;
+        cout << "The address is first index (8) is : " << &arr[0] << endl;
 
     // *(arr) --> value at zero th location.
         cout << "value of first element " << *arr << endl;
@@ -48,12 +52,15 @@ int main()
         cout << "2th " << *(arr+2) << endl;
 
         int i = 3;
-        cout << "3th " << i[arr] << endl;
-         */
-    /*
-       int temp[20] = {45,1,2,5,4};
+         cout << "3th " << i[arr] << endl;
+    */
+
+
+    /* 
+       int temp[10] = {45,1,2,5,4};
        cout << "size of temp is : " << sizeof(temp) << endl;
-       cout << "size of temp is : " << &temp << endl;
+       cout << "address of zero index in temp is : " << &temp << endl;
+       cout << "address of zero index in temp is : " << temp << endl;
        cout <<"size of first index element is " << sizeof(temp[0]) << endl;
        cout <<"size of first index element is " << sizeof(*(temp)) << endl;
 
@@ -61,39 +68,43 @@ int main()
        cout << "size of *p is : " << sizeof(p) << endl;
        cout << "size of *p is : " << sizeof(*p) << endl;
        cout << "address of *p is (address of zeroth index): " << (&p) << endl;
-    */
+       cout << "address of *p is (address of first index): " << (&p +1) << endl;
+     */
 
-    /*    int a[12] = {1, 2, 0, 21, 26, 27, 30, 31, 32};
+/* 
+       int a[12] = {1, 2, 0, 21, 26, 27, 30, 31, 32};
        int *b = &a[0];
 
        cout << "1th " << &a[0] << endl;
        cout << "2nd " << &a << endl;
        cout << "3nd --> " << a << endl;
 
-       cout << "3rd " << *b << endl;
-       cout << "3rd --> " << &b << endl;
+       cout << "printing 1st --> " << *b << endl;
+       cout << "address of 1st pointer --> " << &b << endl;
        cout << "3rd --> " << &*b << endl;
-       cout << "3rd " << b << endl; */
+       cout << "3rd " << b << endl; 
+ */
 
-    /*
+    /* 
         int a[10];
 
         // a  = a + 1;  //* give error
         // cout << a << endl;
 
     //* not gives error
-        int *p = &a[0];
+        int *p = &a[10];
         cout << p << endl;
         p = p + 1;
         cout << p << endl;
-     */
-    /*
-    //& character array ..
+     */ 
 
+    
+    //& character array ..
+/* 
         int arr[5] = {1, 2, 3, 4, 5};
         cout << arr << endl; // printing address of zeroth index which is 1
 
-        char ar[] = "abcd";
+        char ar[] = "abcasdf";
         cout << ar << endl; // printing the all string which is abcd
 
         char *p = &ar[0];
@@ -102,17 +113,17 @@ int main()
         char temp = 'e';
         cout << temp << endl;
         char *p1 = &temp;
-        cout << p1 << endl;
+        cout << p1 << endl; // check this very interesting thing
+ */
+     
 
-     */
-
-    /*
-      //* pointer and function */
+    
+    //* pointer and function */
 
     int a = 10;
     int *p = &a;
     cout << "outside the function " << p << endl;
-    cout << "outside the function " << *p << endl;
+    cout << "outside the function " << *p << endl << endl;
     print(p);
 
     cout << endl;
