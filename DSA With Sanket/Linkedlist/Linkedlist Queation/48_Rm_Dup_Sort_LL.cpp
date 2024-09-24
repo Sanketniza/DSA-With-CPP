8abcf7eb36375a08399e402c7dc4177347460366
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-09-24 12:20:31 +0530

    removing duplicte element in LL , where LL is sorted or unsorted with multiple apporch.

---
+++ DSA With Sanket/Linkedlist/Linkedlist Queation/48_Rm_Dup_Sort_LL.cpp
@@ -0,0 +1,144 @@
+
+/* 
+    lecture 48 : Remove Duplicates from Sorted Linked List
+   todo : Remove Duplicates from Sorted Linked List
+
+   todo : link :- https://www.naukri.com/code360/problems/unique-sorted-list_2420283?leftPanelTab=0%3Fsource%3Dyoutube&campaign=YouTube_Lovebabbar29thJan&leftPanelTabValue=PROBLEM
+
+   Problem statement
+A doubly-linked list is a data structure that consists of sequentially linked nodes, and the nodes have reference to both the previous and the next nodes in the sequence of nodes.
+
+
+
+You are given a sorted doubly linked list of size 'n'.
+
+
+
+Remove all the duplicate nodes present in the linked list.
+
+
+
+Example :
+Input: Linked List: 1 <-> 2 <-> 2 <-> 2 <-> 3
+
+Output: Modified Linked List: 1 <-> 2 <-> 3
+
+Explanation: We will delete the duplicate values ‘2’ present in the linked list.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+5
+1 2 2 2 3
+
+
+Sample Output 1 :
+1 2 3
+
+
+Explanation For Sample Input 1 :
+We will delete the duplicate values ‘2’ present in the linked list.
+
+
+Sample Input 2 :
+4
+1 2 3 4
+
+
+Sample Output 2 :
+1 2 3 4
+
+
+Explanation For Sample Input 2 :
+The list contains no duplicates, so the final list is unchanged.
+
+
+Expected time complexity :
+The expected time complexity is O('n').
+
+
+Constraints :
+1 <= 'n' <= 10^5
+1 <= 'data' in any node <= 10^6
+
+Time limit: 1 sec
+
+ */
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void removeDuplicates(Node* &head) {
+
+    // Empty LL
+    if(head == NULL) {
+        return;
+    }
+
+    // Non-Empty LL
+    Node *curr = head ;
+
+    while (curr != NULL) {
+
+        if ( (curr -> next != NULL) && (curr -> data == curr -> next -> data)) {
+
+            Node *next_next = curr -> next -> next;
+            Node *nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
+        }
+        else {
+            curr = curr -> next;
+        }
+    }
+    // return head;
+}
+
+
+void print (Node* &head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main() {
+
+// todo: only for sorted LL
+
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(20);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(40);
+    Node* sixth = new Node(60);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = NULL;
+
+    print(head);
+    
+    
+    removeDuplicates(head);
+    print(head);
+
+
+ return 0;
+}
---
+++ DSA With Sanket/Linkedlist/Linkedlist Queation/48_Rm_Dup_Unsorted_LL.cpp
@@ -0,0 +1,139 @@
+/* 
+   !lecture 48: Remove Duplicates from Unsorted Linked List
+   link : https://www.naukri.com/code360/problems/remove-duplicates-from-unsorted-linked-list_1069331?source=youtube&campaign=YouTube_Lovebabbar29thJan
+
+   Q1: Remove Duplicates from Unsorted Linked List
+
+   Q2: Remove Duplicates from Sorted Linked List
+
+   Q3: Remove Duplicates from Circular Linked List
+
+   Q4: Remove Duplicates from Doubly Linked List
+
+
+? Remove Duplicates From an Unsorted Linked List
+Moderate
+
+Problem statement
+   ^ You are given a linked list of N nodes. Your task is to remove the    ^ duplicate nodes from the linked list such that every element in the    ^ linked list occurs only once i.e. in case an element occurs more than    ^ once, only keep its first occurrence in the list.
+   ^ 
+   ^ For example :
+   ^ Assuming the linked list is 3 -> 2 -> 3 -> 4 -> 2 -> 3 -> NULL.
+   ^ 
+   ^ Number ‘2’ and ‘3’ occurs more than once. Hence we remove the    ^ duplicates and keep only their first occurrence. So, our list becomes :    ^ 3 -> 2 -> 4 -> NULL.
+   ^ Detailed explanation ( Input/output format, Notes, Images )
+   ^ Constraints :
+   ^ 1 <= T <= 100   
+   ^ 1 <= N <= 10 ^ 4
+   ^ 1 <= data <= 10 ^ 5 
+   ^ 
+   ^ Where ‘N’ is the number of nodes in the list and 'data' is the value of    ^ list nodes.
+   ^ 
+   ^ Time Limit: 1sec
+   ^ Sample Input 1 :
+   ^ 2
+   ^ 4 2 5 4 2 2 -1
+   ^ 1 2 1 2 2 2 7 7 -1
+   ^ Sample Output 1 :
+   ^ 4 2 5 -1
+   ^ 1 2 7 -1
+   ^ Explanation of Sample Input1 :
+   ^ For the first test case, the linked list is 4 -> 2 -> 5 -> 4 -> 2 -> 2    ^ -> NULL. Number ‘4’ and ‘2’ occurs more than once. Hence, we remove the    ^ duplicates and keep only their first occurrence. So, our list becomes :    ^ 4 -> 2 -> 5 -> NULL.
+   ^ 
+   ^ For the second test case, the linked list is 1 -> 2 -> 1 -> 2 -> 2 -> 2    ^ -> 7 -> 7 -> NULL. Number ‘1’, ‘2’ and ‘7’ occurs more than once.    ^ Hence, we remove the duplicates and keep only their first occurrence.    ^ So, our list becomes : 1 -> 2 -> 7 -> NULL.
+   ^ Sample Input 2 :
+   ^ 2
+   ^ 3 3 3 3 3 -1
+   ^ 10 20 10 20 30 10 20 30 -1
+   ^ Sample Output 2 :
+   ^ 3 -1
+   ^ 10 20 30 -1
+
+
+
+ */
+
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void removeDuplicates(Node* &head) {
+
+    if(head == NULL) {
+        return;
+    }
+
+    Node* curr = head;
+
+    while (curr != NULL) {
+
+        Node* temp = curr -> next;
+        Node* prev = curr; // Add this line
+
+        while (temp != NULL) {
+
+            if (curr -> data == temp -> data) {
+
+                Node* nodeToDelete = temp;
+                temp = temp -> next;
+                prev -> next = temp; // Change this line
+                delete(nodeToDelete);
+
+            } else {
+
+                prev = temp; // Add this line
+                temp = temp -> next;
+            }
+            
+        }
+
+             curr = curr -> next;
+    }
+}
+
+void print(Node* &head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << "  ->  ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+
+    Node* head = new Node(10);
+    Node* second = new Node(60);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+    Node* fifth = new Node(10);
+    Node* sixth = new Node(60);
+    Node* seventh = new Node(30);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = seventh;
+    seventh->next = NULL;
+
+    print(head);
+
+
+    removeDuplicates(head);
+    print(head);
+
+
+ return 0;
+}
