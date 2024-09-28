3ae69e93b6f96a410ccdb4465cab66004dafcfb0
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-09-28 19:21:16 +0530

    Flatten a Linked List

--- DSA With Sanket/Linkedlist/Linkedlist Queation/52_Clone_Of_LL.cpp
+++ DSA With Sanket/Linkedlist/Linkedlist Queation/52_Clone_Of_LL.cpp
@@ -1,5 +1,40 @@
-#include<iostream>
+/* 
+    Clone a linked list with random pointer
+    link- 
 
+    Approach:   
+    1. Create a map to store the mapping of original nodes to their corresponding cloned nodes.
+    2. Traverse the original linked list and create a new node for each original node.
+    3. Set the next and random pointers for the cloned nodes using the map.
+    4. Return the head of the cloned linked list.
+
+    TC - O(N)
+    SC - O(N)
+
+    Note: This approach assumes that the random pointer points to a node in the original linked list.
+    If the random pointer points to a node outside the original linked list, the approach will not work.
+
+    Edge Case:
+    If the linked list is empty, return NULL.
+    If the linked list has only one node, return the same node as the cloned node.
+
+    Example:
+    Input: 1 -> 2 -> 3 -> 4 -> 5
+    Output: 1 -> 2 -> 3 -> 4 -> 5
+
+    Input: 1 -> 2 -> 3 -> 4 -> 5
+    Output: 1 -> 2 -> 3 -> 4 -> 5
+
+    Input: 1 -> 2 -> 3 -> 4 -> 5
+    Output: 1 -> 2 -> 3 -> 4 -> 5
+
+    Input: 1 -> 2 -> 3 -> 4 -> 5
+    Output: 1 -> 2 -> 3 -> 4 -> 5
+
+
+*/
+
+#include<iostream>
 using namespace std;
 
 class  Node{
---
+++ DSA With Sanket/Linkedlist/Linkedlist Queation/53_Flatten_LL.cpp
@@ -0,0 +1,149 @@
+/*
+ *  lecture 53 : Flatten a Linked List
+ *  link -https://www.naukri.com/code360/problems/flatten-a-linked-list_1112655?source=youtube&campaign=YouTube_CodestudioLovebabbar5thfeb
+
+   Problem statement
+You are given a linked list containing 'n' 'head' nodes, where every node in the linked list contains two pointers:
+
+
+
+(1) ‘next’ which points to the next node in the list
+
+(2) ‘child’ pointer to a linked list where the current node is the head.
+
+
+
+Each of these child linked lists is in sorted order and connected by 'child' pointer.
+
+
+
+Your task is to flatten this linked such that all nodes appear in a single layer or level in a 'sorted order'.
+
+
+
+Example:
+Input: Given linked list is:
+
+Output:
+1 → 2 → 3 → 4 → 5 → 6 → 7 → 8 → 9 → 12 → 20 → null.
+ */
+
+#include <iostream>
+using namespace std;
+
+// Definition for linked list node
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *child;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->child = NULL;
+    }
+};
+
+Node *mergeTwoList(Node *first, Node *second)
+{
+
+       // Base cases: if one list is empty, return the other
+    if (first == NULL) {
+      return second;
+    }
+    if ( second == NULL ) {
+      return first;
+    }
+
+    // Choose the smaller head as the new head
+    Node *result;
+
+    if (first->data <= second->data) {
+        result = first;
+        result->child = mergeTwoList(first->child, second);
+    }
+
+    else {
+        result = second;
+        result->child = mergeTwoList(first, second->child);
+    }
+
+    // Set next to NULL as we're flattening the list
+    result->next = NULL;
+    return result;
+}
+
+Node *flattenLinkedList(Node *head)
+{
+
+    if (!head || !head->next)
+    {
+        return head;
+    }
+
+    // recurse for list on right
+    head->next = flattenLinkedList(head->next);
+
+    // merge two list
+    head = mergeTwoList(head, head->next);
+
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->child;  // Change this line from temp = temp->next to temp = temp->child
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *one = new Node(1);
+    Node *two = new Node(4);
+    Node *three = new Node(7);
+    Node *four = new Node(9);
+    Node *five = new Node(11);
+
+    Node *head = one;
+
+    one->next = two;
+    two->next = three;
+    three->next = four;
+    four->next = five;
+    five->next = NULL;
+
+    Node *six = new Node(2);
+    Node *seven = new Node(3);
+
+    one->child = six;
+    six->next = seven;
+
+    Node *eight = new Node(5);
+    Node *nine = new Node(6);
+
+    two->child = eight;
+    eight->next = nine;
+
+    Node *ten = new Node(8);
+    three->child = ten;
+
+    Node *eleven = new Node(10);
+    four->child = eleven;
+
+    // flattenLinkedList
+    Node* ans = flattenLinkedList(head);
+
+    print(ans);
+
+    return 0;
+}
