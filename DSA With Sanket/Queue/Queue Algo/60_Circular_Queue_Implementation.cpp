60113c3ed7944e4e6c47f87f7fbb34ff43f583b1
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2024-10-05 13:09:38 +0530

    circular queue

---
+++ DSA With Sanket/Queue/Queue Algo/60_Circular_Queue_Implementation.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Queue {
+
+    int *arr;
+    int front;
+    int rear;
+    int size;
+
+    public:
+        Queue(int value) {
+             size = 10001;
+             arr = new int[size]; 
+             front = rear = -1;
+        }
+
+        int push() {
+
+        }
+
+        int pop() {
+
+        }
+
+        int empyt() {
+            
+        }
+};
+
+int main() {
+
+
+ return 0;
+}
