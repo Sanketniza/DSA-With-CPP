b8b5a6673e45fa00af92d6218c6e9dd00541d0e0
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-01-25 19:48:44 +0530

    Add Depth First Search (DFS) implementation; includes adjacency list creation and example usage.

---
+++ DSA With Sanket/Graph/Algorithm/87_Implementation_DFS.cpp
@@ -0,0 +1,75 @@
+/* 
+    ? Depth First Search (DFS)
+    ? 
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+void dfs(int node, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj, vector<int> &component) {
+    component.push_back(node);
+    visited[node] = true;
+
+    // For each connected node, make a recursive call
+    for (auto i : adj[node]) {
+        if (!visited[i]) {
+            dfs(i, visited, adj, component);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges) {
+    // Prepare the adjacency list
+    unordered_map<int, list<int>> adj;
+
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    unordered_map<int, bool> visited;
+    vector<vector<int>> ans;
+
+    // Perform DFS for each node
+    for (int i = 0; i < V; i++) { // V -> Total number of nodes in graph
+        if (!visited[i]) {
+            vector<int> component;
+            dfs(i, visited, adj, component);
+            ans.push_back(component);
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int V = 10, E = 9;
+    vector<vector<int>> edges = {
+        {1, 0},
+        {2, 0},
+        {3, 0},
+        {4, 0},
+        {5, 0},
+        {6, 0},
+        {7, 0},
+        {8, 0},
+        {9, 0}
+    };
+
+    vector<vector<int>> result = depthFirstSearch(V, E, edges);
+
+    for (const auto &component : result) {
+        for (int node : component) {
+            cout << node << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
