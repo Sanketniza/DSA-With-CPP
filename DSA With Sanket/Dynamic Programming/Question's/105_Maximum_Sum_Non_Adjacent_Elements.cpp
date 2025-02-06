d0281c788ddfbea31a3de96bf14fa8afd195d1cd
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-06 20:03:25 +0530

    Add multiple approaches for Maximum Sum of Non-Adjacent Elements problem with detailed comments

--- DSA With Sanket/Dynamic Programming/Question's/105_Maximum_Sum_Non_Adjacent_Elements.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/105_Maximum_Sum_Non_Adjacent_Elements.cpp
@@ -47,10 +47,112 @@ In test case 2, out of all the possibilities, if we take the sum of 'ARR[0]', 'A
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+int solve(vector<int> &nums , int n) {
+
+    // base case 
+    if(n == 0) {
+        return nums[0];
+    }
+
+    if(n < 0) {
+        return 0;
+    }
+
+    int inclu = solve(nums , n - 2) + nums[n];
+    int exclu = solve(nums , n - 1) + 0;
+
+    return max(inclu , exclu);
+}
+
+int solve1(vector<int> &nums , int n , vector<int> &dp) {
+
+    // base case 
+
+    if(n == 0){
+        return nums[0];
+    }
+
+    if(n < 0) {
+        return 0;
+    }
+
+    if(dp[n] != -1) {
+        return dp[n];
+    }
+
+    // recursive call
+
+    int inclu = solve1(nums , n - 2 , dp) + nums[n];
+    int exclu = solve1(nums , n - 1 , dp) + 0;
+
+    dp[n] = max(inclu , exclu);
+
+    return dp[n];
+}
+
+int solve2(vector<int> &nums ) {
+    int n = nums.size();
+
+    vector<int> dp(n , 0);
+
+    dp[0] = nums[0];
+
+    // base case
+    if(n < 0) {
+        return 0;
+    }
+
+    for(int i = 1; i < n; i++) {
+        int inclu = dp[i - 2] + nums[i];
+        int exclu = dp[i - 1] + 0;
+        dp[i] = max(inclu , exclu);
+    }
+
+    return dp[n -1];
+}
+
+int solve3(vector<int> &nums) {
+
+    int n = nums.size();
+
+    int a = 0;
+    int b = nums[0];
+
+    for(int i = 1; i < n; i++) {
+        int inclu = a + nums[i];
+        int exclu = b + 0;
+
+        int ans = max(inclu , exclu);
+        a = b;
+        b = ans;
+    }
+
+    return b;
+}
  
 int main() { 
 
-    
+    vector<int> nums = {1, 2, 3, 5, 4};
+    int n = nums.size();
+
+     // way 1 = recursive
+    // int n = nums.size();
+    // int ans = solve(nums , n - 1);
+    // return ans;
+
+   // way 2 = R + M
+
+    // int n = nums.size();
+    // vector<int> dp(n , -1);
+    // int ans = solve1(nums , n - 1 , dp);
+    // return ans;
+
+    // way 3 = T
+    // return solve2(nums);
+
+    // way 4 = Space Opt
+      //  return solve3(nums);
 
     return 0;
 }
