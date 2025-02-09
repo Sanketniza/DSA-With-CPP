3c672fec037be3a163e4b5b1fb75b5b21bb45254
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-09 12:59:25 +0530

    Refactor code formatting in 0/1 Knapsack Problem implementation

--- DSA With Sanket/Dynamic Programming/Algorithm's/110_01_KnapSack_Problem.cpp
+++ DSA With Sanket/Dynamic Programming/Algorithm's/110_01_KnapSack_Problem.cpp
@@ -34,8 +34,6 @@ using namespace std;
 
 int main() {
 
-
- 
-
+    
  return 0;
 }
---
+++ DSA With Sanket/Dynamic Programming/Question's/113_Minimum_Cost_for_Tickets.cpp
@@ -0,0 +1,70 @@
+/* 
+    113: Minimum Cost for Tickets
+    https://www.naukri.com/code360/problems/minimum-coins_2180776?leftPanelTab=0
+
+     Ninja's Trip
+Moderate
+80/80
+Contributed by
+48 upvotes
+Asked in companies
+Problem statement
+Ninja is willing to take some time off from his training and planning a year-long tour. You are given a DAYS array consisting of ‘N’ days when Ninjas will be traveling during the year. Each Day is an integer between 1 to 365 (both inclusive). Train tickets are sold in three different ways:
+A 1-day pass is sold for 'COST'[0] coins,
+A 7-day pass is sold for 'COST'[1] coins, and
+A 30-day  pass is sold for 'COST'[2] coins.
+The passes allow for many days of consecutive travel.
+Your task is to help the Ninja to find the minimum number of coins required to complete his tour. For example,
+If Ninja gets a 7-day pass on day 2, then he can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+1 <= T <= 10
+1 <= N <= 365
+1 <= DAYS[i] <= 365
+
+Time Limit: 1 sec
+Sample Input 1:
+2
+2 
+2 5
+1 4 25    
+7
+1 3 4 5 7 8 10
+2 7 20
+Sample Output 1:
+2
+11
+Explanation For sample input 1:
+For the first test case, 
+On Day 2, Ninja will buy a 1-day pass with 1 coin.
+On Day 5, Ninja will buy a 1-day pass with 1 coin.
+In total, Ninja will spend 2 coins. Hence the answer is 2.
+
+For the second test case,
+On Day 1, Ninja will buy a 1-day pass with 2 coins.
+On Day 3, Ninja will buy a 7-day pass with 7 coins valid for days 3,4,5...9.
+On Day 10, Ninja will buy a 1-day pass with 2 coins.
+In total, Ninja will spend 11 coins. Hence the answer is 11.
+Sample Input 2:
+2
+6
+1 4 6 7 8 20
+2 7 15
+12
+1 2 3 4 5 6 7 8 9 10 30 31
+2 7 15 
+Sample Output 2:
+11
+17
+
+ */
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+ return 0;
+} 
