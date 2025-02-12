2473f5d3a77671861466fc2c452008d919ea9e79
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-12 18:26:23 +0530

    Add recursive implementation for 0/1 Knapsack problem with example case

--- DSA With Sanket/Dynamic Programming/Algorithm's/110_01_KnapSack_Problem.cpp
+++ DSA With Sanket/Dynamic Programming/Algorithm's/110_01_KnapSack_Problem.cpp
@@ -31,9 +31,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int solve(vector<int> &weight , vector<int> &value , int capacity , int index) {
+
+    // base case
+    // if only one item to steel , then just compare it's weight with the knapsack capacity
+
+    if(index == 0 ) {
+       if(weight[0] <= capacity) {
+          return value[0];
+       } 
+       
+       else {
+           return 0;
+       }
+    }
+
+    // if the weight of the item is greater than the capacity of the knapsack
+    // then we can't steel that item
+
+    int include = 0;
+
+    if(weight[index] <= capacity) {
+        include = value[index] + solve(weight , value , capacity - weight[index] , index - 1);
+    }
+
+    int exclude = 0 + solve(weight , value , capacity , index - 1);
+
+    return max(include , exclude);
+
+}
+
+int knapsack(vector<int> &weight , vector<int> &value , int W , int n) {
+
+   //todo: recursive approach
+    int ans = solve(weight , value , W , n - 1  );
+    cout << "ans is : " << ans << endl;
+
+    // todo: DP approach
+    vector<vector<int>> dp(n + 1 , vector<int>(W + 1 , -1));
+    int ans1 = solveM(weight , value , W , n , dp);
+    cout << "ans1 is : " << ans1 << endl;
+
+    // todo: Tabulation approach
+    int ans2 = solveTab(weight , value , W , n);
+}
+
 
 int main() {
 
+    vector<int> weight = {1, 2, 4, 5};
+    vector<int> value = {5, 4, 8, 6};
+
+    int n = weight.size();
+    int W = 5;
+
+    int result = knapsack(weight, value, W, n);
     
  return 0;
 }
