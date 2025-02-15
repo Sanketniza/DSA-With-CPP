550034e706394c4349fc6be6b32dc826fce923da
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-15 13:14:56 +0530

    Add initial implementation for Partition Equal Subset Sum problem

---
+++ DSA With Sanket/Dynamic Programming/Question's/123_Partition_Equal_Subset_Sum.cpp
@@ -0,0 +1,27 @@
+/* 
+    ? 123: Partition Equal Subset Sum
+    ? https://www.geeksforgeeks.org/problems/subset-sum-problem2014/1
+
+    Given an array arr[], return true if it can be partitioned into two subsets such that the sum of elements in both parts is the same, otherwise, false.
+    Note: Each element is present in either the first subset or the second subset, but not in both.
+
+    Examples:
+
+    Input: arr = [1, 5, 11, 5]
+    Output: true
+    Explanation: The two parts are [1, 5, 5] and [11].
+    Input: arr = [1, 3, 5]
+    Output: false
+    Explanation: This array can never be partitioned into two such parts.
+
+ */
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  
+ return 0;
+} 
