04e2652c2d5f2025d847d19081932b902575da93
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-16 19:33:49 +0530

    Add optimized recursive and dynamic programming solutions for Unique Binary Search Trees problem

--- DSA With Sanket/Dynamic Programming/Question's/127_Unique_Binary_Search_Trees.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/127_Unique_Binary_Search_Trees.cpp
@@ -40,6 +40,42 @@ int solve(int n ) {
     return ans;
 } 
 
+int solveM(int n , vector<int> &dp ) {
+
+    if(n <= 1)
+        return 1;
+
+    if(dp[n] != -1)
+        return dp[n];
+
+    int ans = 0;
+    for(int i = 1; i <= n; i++) {
+        ans += solveM(i - 1 , dp) * solveM(n - i , dp);
+    }
+
+    return dp[n] = ans;
+}
+
+int solveT(int n) {
+
+    vector<int> dp(n + 1 , 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    // i -> number of nodes
+
+    for(int i = 2; i <= n; i++) {
+
+        // j -> root node
+
+        for(int j = 1; j <= i; j++) {
+            dp[i] += dp[j - 1] * dp[i - j];
+        }
+
+    }
+    return dp[n];
+}
 int main() {
 
     int n = 3;
@@ -47,5 +83,15 @@ int main() {
     //todo: recursive approach
     int a = solve(n);
     cout << "ans is : " << a << endl;
+
+    //todo: dp approach( bottom up)
+    vector<int> dp(n + 1 , -1);
+    int b = solveM(n , dp);
+    cout << "ans is : " << b << endl;
+
+    //todo: dp approach( top down)( optimized)
+    int c = solveT(n);
+    cout << "ans is : " << c << endl;
+
  return 0;
 } 
