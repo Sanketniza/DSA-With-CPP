5fb6340c46b1daf0c767b91de7f500955d79d0ff
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-18 19:18:02 +0530

    Add memoization approach for Buy and Sell Stock problem

--- DSA With Sanket/Dynamic Programming/Question's/131_Buy_and_Sell_Stock.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/131_Buy_and_Sell_Stock.cpp
@@ -46,19 +46,57 @@ int solve(int index , int buy , vector<int> price) {
     // recursive case
     int profit = 0;
 
+    // if we can buy the stock
     if(buy){
+        // we can either buy the stock or not buy the stock
         int buyKiya = -price[index] + solve(index + 1 , 0 , price);
         int nahiKiya = solve(index + 1 , 1 , price);
         profit = max(buyKiya , nahiKiya);
     }
 
+    // if we can sell the stock
     else {
+        // we can either sell the stock or not sell the stock
         int sellKiya = price[index] + solve(index + 1 , 1 , price);
         int nahiKiya = solve(index + 1 , 0 , price);
         profit = max(sellKiya , nahiKiya);
     }
 
     return profit;
+}
+
+int sovleM(int index , int buy , vector<int> price , vector<vector<int>> &dp) {
+
+    // base case
+    if(index >= price.size()) {
+        return 0;
+    }
+
+    // recursive case
+    if(dp[index][buy] != -1) {
+        return dp[index][buy];
+    }
+
+    int profit = 0;
+
+    // if we can buy the stock
+    if(buy){
+        // we can either buy the stock or not buy the stock
+        int buyKiya = -price[index] + sovleM(index + 1 , 0 , price , dp);
+        int nahiKiya = sovleM(index + 1 , 1 , price , dp);
+        profit = max(buyKiya , nahiKiya);
+    }
+
+    // if we can sell the stock
+    else {
+        // we can either sell the stock or not sell the stock
+        int sellKiya = price[index] + sovleM(index + 1 , 1 , price , dp);
+        int nahiKiya = sovleM(index + 1 , 0 , price , dp);
+        profit = max(sellKiya , nahiKiya);
+    }
+
+    // store the result in dp array
+    return dp[index][buy] = profit;
 }
 
 int main() {
@@ -69,6 +107,11 @@ int main() {
     //todo: Recursive Approach
     int a = solve(0 , 1 , prices); //  1 -> allowed to buy , 0 - > not allowed to buy 
     cout << "Max Profit is : " << a << endl;
+
+    //todo: Top Down Approach (Memoization + Recursion)
+    vector<vector<int>> dp(n , vector<int>(2 , -1));
+    int b = sovleM(0 , 1 , prices , dp);
+    cout << "Max Profit is : " << b << endl;
 
  return 0;
 } 
