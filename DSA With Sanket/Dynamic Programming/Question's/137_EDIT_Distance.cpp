d352352fb2f7bcd66dd28f23b60a1ecb7f8c370e
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-20 10:08:39 +0530

    Add recursive solution for Edit Distance problem

--- DSA With Sanket/Dynamic Programming/Question's/137_EDIT_Distance.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/137_EDIT_Distance.cpp
@@ -36,8 +36,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+    int solve(string& a , string &b , int i , int j) {
+
+        //base case
+        if( i == a.length())
+            return b.length() - j;
+
+        if( j == b.length())
+            return a.length() - i;
+
+        // recursive call
+
+        int ans = 0;
+
+        if(a[i] == b[j]){
+            ans = solve(a , b , i + 1 , j + 1);
+        } 
+
+        else{
+            // insert
+            int insertAns = 1 + solve(a , b , i , j + 1);
+            // delete
+            int deleteAns = 1 + solve(a , b , i + 1 , j);
+            // replace
+            int replaceAns = 1 + solve(a , b , i + 1 , j +1);
+
+            ans = min(insertAns , min(deleteAns , replaceAns));
+        }      
+
+        return ans; 
+    }
+
 int main() {
 
+    string word1 = "horse";
+    string word2 = "ros";
+
+    int n = word1.length();
+    int m = word2.length();
+
+    //todo: Recursive Approach
+        int a = solve(word1 , word2 , 0 , 0);
+        cout << "ans is : " << a << endl;
+
+    //todo: Memoization Approach
+        //vector<vector<int>> dp(n , vector<int> (m , -1));
+        //int b = solveM(word1 , word2 , 0 , 0 , dp);
+        //cout << "ans is : " << b << endl;    
 
  return 0;
 } 
