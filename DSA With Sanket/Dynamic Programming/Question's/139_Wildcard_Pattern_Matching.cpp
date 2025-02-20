97b839029bbde5bc629467d181c78082d64ddce5
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-20 11:06:40 +0530

    Add initial implementation for Wildcard Pattern Matching problem

---
+++ DSA With Sanket/Dynamic Programming/Question's/139_Wildcard_Pattern_Matching.cpp
@@ -0,0 +1,39 @@
+/* 
+    ? 139: Wildcard Pattern Matching
+    ? https://leetcode.com/problems/wildcard-matching/description/
+
+    Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+
+    '?' Matches any single character.
+    '*' Matches any sequence of characters (including the empty sequence).
+    The matching should cover the entire input string (not partial).
+
+    
+
+    Example 1:
+
+    Input: s = "aa", p = "a"
+    Output: false
+    Explanation: "a" does not match the entire string "aa".
+    Example 2:
+
+    Input: s = "aa", p = "*"
+    Output: true
+    Explanation: '*' matches any sequence.
+    Example 3:
+
+    Input: s = "cb", p = "?a"
+    Output: false
+    Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+ */
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+ return 0;
+}
