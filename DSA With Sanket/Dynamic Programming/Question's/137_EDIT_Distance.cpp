6f305f652be8e6f37a91a6b4557669d096228206
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-02-20 10:16:51 +0530

    Add tabulation and space optimized approaches for Edit Distance problem

--- DSA With Sanket/Dynamic Programming/Question's/137_EDIT_Distance.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/137_EDIT_Distance.cpp
@@ -101,7 +101,7 @@ using namespace std;
             //* 3. replace the character from string a at the current position with the character from string b at the current position
 
             // we take the minimum of these three options
-            
+
             // insert
             int insertAns = 1 + solveM(a , b , i , j + 1 , dp);
             // delete
@@ -114,6 +114,102 @@ using namespace std;
 
         // store the result in the dp array
         return dp[i][j] = ans;
+    }
+
+    int solveTabulation(string &a , string &b) {
+
+        int n = a.length();
+        int m = b.length();
+        vector<vector<int>> dp(n + 1 , vector<int> (m + 1 , 0));
+
+        //base cases
+
+        // if we have reached the end of string a, the remaining characters in string b are to be inserted
+        for(int j = m - 1; j >= 0; j--) {
+            dp[n][j] = m - j;
+        }
+
+        // if we have reached the end of string b, the remaining characters in string a are to be deleted
+        for(int i = n - 1; i >= 0; i--) {
+            dp[i][m] = n - i;
+        }
+
+        // fill the dp array
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+
+                int ans = 0;
+
+                if(a[i] == b[j]) {
+                    // if the characters at the current positions in both strings are the same, move to the next positions
+                    dp[i][j] = dp[i + 1][j + 1];
+                    // ans = dp[i + 1][j + 1];
+                }
+
+                else {
+                    // if the characters at the current positions in both strings are different, we have three options:
+                    //* 1. insert the character from string b at the current position in string a
+                    //* 2. delete the character from string a at the current position
+                    //* 3. replace the character from string a at the current position with the character from string b at the current position
+
+                    // we take the minimum of these three options
+
+                    // insert
+                    int insertAns = 1 + dp[i][j + 1];
+                    // delete
+                    int deleteAns = 1 + dp[i + 1][j];
+                    // replace
+                    int replaceAns = 1 + dp[i + 1][j + 1];
+
+                    dp[i][j] = min(insertAns , min(deleteAns , replaceAns));
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    int solveTabulationSpaceOptimized(string &a , string &b ) {
+
+        int n = a.length();
+        int m = b.length();
+
+        vector<int> curr(n + 1 , 0);
+        vector<int> next(n + 1 , 0);
+
+        // remaining base cases
+        for (int j = 0; j <= m; j++) {
+            next[j] = m - j;
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+
+                 curr[m] = n - i;
+
+            int ans = 0;
+
+            if (a[i] == b[j]) {
+                ans = next[j + 1];
+            } else {
+                // insert
+                int insertAns = 1 + curr[j + 1];
+                // delete
+                int deleteAns = 1 + next[j];
+                // replace
+                int replaceAns = 1 + next[j + 1];
+
+                ans = min(insertAns, min(deleteAns, replaceAns));
+            }
+
+            curr[j] = ans;
+        }
+
+        next = curr;
+    }
+
+    return next[0];
     }
 
 int main() {
@@ -132,6 +228,14 @@ int main() {
         vector<vector<int>> dp(n , vector<int> (m , -1));
         int b = solveM(word1 , word2 , 0 , 0 , dp);
         cout << "ans is : " << b << endl;    
+
+     //todo: Tabulation approach (bottom up )
+        int c = solveTabulation(word1 , word2);
+        cout << "ans is : " << c << endl;  
+
+     //todo: spaceOptimized Tabulation approach (bottom up )
+        int d = solveTabulationSpaceOptimized(word1 , word2);
+        cout << "ans is : " << d << endl;    
 
  return 0;
 } 
