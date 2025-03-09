9cae2d54065d228271a1ebffe7d271a49a765a94
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-03-09 12:50:24 +0530

    Add stack implementation using dynamic array; refactor existing stack class for consistency

--- DSA With Sanket/Stack/Stack Algo/54_Stack_Array_Impl.cpp
+++ DSA With Sanket/Stack/Stack Algo/54_Stack_Array_Impl.cpp
@@ -5,66 +5,67 @@ using namespace std;
 
 class Stack
 {
-public:
-    int *arr;
-    int top;
-    int size;
+    public:
+        int *arr; // because we are using dynamic memory allocation so we need to use pointer.
+        int top;
+        int size;
 
-    Stack(int s)
-    {
-        this->size = s;
-        arr = new int[size];
-        top = -1;
-    }
+        Stack(int s)
+        {
+            this->size = s;
+            arr = new int[size];
+            top = -1;
+        }
 
-    void push(int element)
-    {
-        if (size - top > 1)
+        void push(int element)
         {
-            top++;
-            arr[top] = element;
+            if (size - top > 1)
+            {
+                top++;
+                arr[top] = element;
+            }
+            
+            else
+            {
+                cout << "Stack Overflow" << endl;
+            }
         }
-        else
-        {
-            cout << "Stack Overflow" << endl;
-        }
-    }
 
-    void pop()
-    {
-        if (top >= 0)
+        void pop()
         {
-            cout << "Popped value is " << arr[top] << endl;
-            top--;
+            if (top >= 0)
+            {
+                cout << "Popped value is " << arr[top] << endl;
+                top--;
+            }
+            else
+            {
+                cout << "Stack Underflow" << endl;
+            }
         }
-        else
-        {
-            cout << "Stack Underflow" << endl;
-        }
-    }
 
-    int peek()
-    {
-        if (top >= 0)
+        int peek()
         {
-            return arr[top];
+            if (top >= 0)
+            {
+                return arr[top];
+            }
+            else
+            {
+                cout << "Stack is empty" << endl;
+                return -1;
+            }
         }
-        else
-        {
-            cout << "Stack is empty" << endl;
-            return -1;
-        }
-    }
 
-    bool isEmpty()
-    {
-        if(top == -1) {
-            return true;
+        bool isEmpty()
+        {
+            if(top == -1) {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
-        else {
-            return false;
-        }
-    }
 };
 
 int main()
---
+++ DSA With Sanket/Stack/Stack Question Practice/4_Stack_Imple_Arr_Vec.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+class stack {
+    
+    public:
+        int *arr;
+        int top;
+        int size;
+
+        stack(int data) {
+            this -> size = data; // size of the stack
+            arr = new int[size]; // dynamic memory allocation for the stack , where size is the size of the stack
+            top = -1; // initially the stack is empty
+        }
+
+        void push(int data) {
+
+            if(size - top > 1) {
+                top++;
+                arr[top] = data;
+            }
+
+            else {
+                cout << "Stack Overflow" << endl;
+            }
+        }
+
+        void pop() {
+            if(top >= 0) {
+                cout << "Popped value is " << arr[top] << endl;
+                top--;
+            }
+
+            else {
+                cout << "Stack Underflow" << endl;
+            }
+        }
+
+        int peek() {
+            if(top >= 0) {
+                return arr[top];
+            }
+
+            else {
+                cout << "Stack is empty" << endl;
+                return -1;
+            }
+        }
+
+        bool isEmpty() {
+            if(top == -1) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        void print() {
+
+            cout << "Stack is: ";
+            for(int i = 0; i <= top; i++) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+
+
+};
+
+int main() {
+
+    stack st(5);
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.print();
+
+    st.pop();
+    st.print();
+
+    cout << "top element is : " << st.peek() << endl;
+
+    cout << "stack is : " << st.isEmpty() << endl;
+
+    if(st.isEmpty()) {
+        cout << "Stack is empty" << endl;
+    }
+
+    else {
+        cout << "Stack is not empty" << endl;
+    }
+
+    st.pop();
+    st.pop();
+
+    if(st.isEmpty()) {
+        cout << "Stack is empty" << endl;
+    }
+
+    else {
+        cout << "Stack is not empty" << endl;
+    }
+
+ return 0;
+}
