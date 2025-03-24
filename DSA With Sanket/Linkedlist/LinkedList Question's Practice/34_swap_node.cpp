a3fdfdbf8670ac2e67bf55973eb79c5870a785b9
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-03-24 15:26:56 +0530

    Add implementation for swapping nodes in pairs in a linked list

---
+++ DSA With Sanket/Linkedlist/LinkedList Question's Practice/34_swap_node.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+
+};
+
+Node *solve(Node *head) {
+
+    if(head || head->next) {
+        return head;
+    }
+
+    Node *dummy = new Node(0);
+    dummy->next = head;
+    Node *prev = dummy;
+    Node *curr = head;
+
+    while (curr || curr -> next) {
+
+        Node *first = curr;
+        Node *second = curr->next;
+
+        prev->next = second;
+        first->next = second->next;
+        second->next = first;
+
+        prev = first;
+        curr = first->next;
+    }
+
+    return dummy->next;
+}
+
+void print(Node* head) {
+
+    Node* temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+int main() {
+
+
+    Node* head = new Node(10);
+    Node* second = new Node(20);
+    Node* third = new Node(30);
+    Node* fourth = new Node(40);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+
+    print(head);
+
+    //* ------------------solution-------------------
+    Node *ans = solve(head);
+    print(ans);
+
+
+ return 0;
+}
