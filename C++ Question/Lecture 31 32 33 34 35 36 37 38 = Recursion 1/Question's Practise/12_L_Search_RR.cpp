c77c282ec188647a6f7df430f63b9d2d4a27cc81
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-03-25 14:52:55 +0530

    Add recursive linear search implementation in C++

---
+++ C++ Question/Lecture 31 32 33 34 35 36 37 38 = Recursion 1/Question's Practise/12_L_Search_RR.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int index , vector<int> &nums1, int target) {
+
+    if(index == nums1.size()) {
+        return -1;
+    }
+
+    if(nums1[index] == target) {
+        cout << "Element found at index: " << index << endl;
+        return index;
+    }
+
+    return solve(index + 1, nums1, target);
+    // return solve(nums1 + 1 , target , size - 1); -> another way , but need to change function parameters
+}
+
+int main() {
+
+    vector<int> nums1 = {1,2,2,1,3,4,5,6,7,8,9,10};
+    int target = 5;
+
+    int ans = solve(0 ,nums1, target);
+    // cout << ans << endl;
+
+ return 0;
+}
