65e1fb0772d91f826b5fa3b3e5108b9030476f0e
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-03-26 13:55:55 +0530

    Add binary tree implementation with build and inorder traversal functions

---
+++ DSA With Sanket/Tree/Binary Tree/Questions Practice/9_BT.cpp
@@ -0,0 +1,63 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Root {
+    public:
+        int data;
+        Root *left;
+        Root *right;
+
+        Root(int d) {
+            this -> data = d;
+            this -> left = NULL;
+            this -> right = NULL;
+        }
+
+};
+
+Root *buildTree(Root *root) {
+
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new Root(data);
+
+    if(data == -1) {
+        return NULL;
+    }
+
+    //left side
+
+    cout << "Enter data for inserting in left of : " << data << endl;
+    root -> left = buildTree(root -> left);
+
+    //right side
+
+    cout << "Enter data for inserting in right of : " << data << endl;
+    root -> right = buildTree(root -> right);
+
+    return root;
+}
+
+void Inorder(Root *root) {
+    if(root == NULL) {
+        return;
+    }
+
+    Inorder(root -> left);
+    cout << root -> data << " ";
+    Inorder(root -> right);
+}
+
+int main() {
+
+    Root *root = NULL;
+
+    root = buildTree(root);
+
+    Inorder(root);
+
+ return 0;
+}
