4b6e9cb00f0fcefafbd576bdd807bd64ad2b354b
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-04-05 12:35:31 +0530

    Add top view of binary tree implementation

---
+++ DSA With Sanket/Tree/Binary Tree/Questions Practice/19_Top_view.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int data) {
+            this -> data = data;
+            this -> left = NULL;
+            this -> right = NULL;
+        }  
+
+};
+
+vector<int> solve(Node *root ) {
+
+    //create ans vector
+    vector<int> ans;
+    
+    if(root == NULL) {
+        return ans;
+    }
+
+
+    //create a map to store the top view
+    map<int , int> topNode;
+
+    // create queue to do level order traversal
+    queue<pair<Node* , int>> q;
+   // push the root node with horizontal distance 0 and level 0
+
+   // push the root node with horizontal distance 0 and level 0
+    q.push(make_pair(root , 0));
+
+    while(!q.empty()) {
+
+        // get the front node and its horizontal distance
+        pair<Node* , int> temp = q.front();
+        q.pop();
+
+        // create a pointer to the front node
+        Node* frontNode = temp.first;
+
+        // get the horizontal distance of the front node
+        int hd = temp.second;
+
+        // if the horizontal distance is not present in the map
+        // then add the front node to the map
+
+        if(topNode.find(hd) == topNode.end()) {
+            topNode[hd] = frontNode -> data;
+        }
+
+        // if the left child is present then push it to the queue
+        if(frontNode -> left) {
+            q.push(make_pair(frontNode -> left , hd - 1));
+        }
+
+        // if the right child is present then push it to the queue
+        if(frontNode -> right) {
+            q.push(make_pair(frontNode -> right , hd + 1));
+        }
+
+    }
+
+    // iterate the map and push the values to the ans vector
+    for(auto i : topNode) {
+        ans.push_back(i.second);
+    }
+
+    return ans;
+
+
+}
+
+int main() {
+
+    Node* root = new Node(1);
+    root -> left = new Node(2);
+    root -> left -> left = new Node(4);
+    root -> left -> right = new Node(5);
+    root -> right = new Node(3);
+    root -> right -> left = new Node(6);
+    root -> right -> right = new Node(7);
+
+    vector <int> ans;
+    ans =  solve(root);
+    cout << "Top view of the binary tree is: ";
+    for(int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+
+
+ return 0;
+}
--- DSA With Sanket/Tree/Binary Tree/Questions/64_Top_View_of_BT.cpp
+++ DSA With Sanket/Tree/Binary Tree/Questions/64_Top_View_of_BT.cpp
@@ -10,7 +10,7 @@
         /     \
        2       3
       /  \    /   \
-    4    5  6   7
+    4    5  6      7
 
     Top view will be: 4 2 1 3 7
     Note: Return nodes from leftmost node to rightmost node. Also if 2 nodes are outside the shadow of the tree and are at same position then consider the left ones only(i.e. leftmost). 
@@ -20,15 +20,15 @@
 
     Input:
           1
-       /    \
-      2      3
+        /   \
+       2     3
     Output: 2 1 3
     Example 2:
 
     Input:
            10
-        /      \
-      20        30
+         /    \
+       20      30
      /   \    /    \
     40   60  90    100
     Output: 40 20 10 30 100
@@ -55,41 +55,41 @@ class Node {
 
 vector<int> topView(Node* root) {
 
-     vector<int> ans;
-     
-        if(root == NULL) 
-        {
-            return ans;
-        }
-        
-        map<int,int> topNode;
-        queue<pair<Node*, int> > q;
-        
-        q.push(make_pair(root, 0));
-        
-        while(!q.empty()) {
+    vector<int> ans;
+    
+    if(root == NULL) 
+    {
+        return ans;
+    }
+    
+    map<int,int> topNode;
+    queue<pair<Node*, int> > q;
+    
+    q.push(make_pair(root, 0));
+    
+    while(!q.empty()) {
 
-            pair<Node*, int> temp = q.front();
-            q.pop();
+        pair<Node*, int> temp = q.front();
+        q.pop();
 
-            Node* frontNode = temp.first;
-            int hd = temp.second;
+        Node* frontNode = temp.first;
+        int hd = temp.second;
+        
+        //if one value is present for a HD, then do nothing
+        if(topNode.find(hd) == topNode.end())
+            topNode[hd] = frontNode -> data;
             
-            //if one value is present for a HD, then do nothing
-            if(topNode.find(hd) == topNode.end())
-                topNode[hd] = frontNode -> data;
-                
-            if(frontNode->left)
-                q.push(make_pair(frontNode->left, hd-1));
+        if(frontNode->left)
+            q.push(make_pair(frontNode->left, hd-1));
 
-            if(frontNode->right)
-                q.push(make_pair(frontNode->right, hd+1));
-        }
-        
-        for(auto i:topNode) 
-        {
-            ans.push_back(i.second);
-        }
+        if(frontNode->right)
+            q.push(make_pair(frontNode->right, hd+1));
+    }
+    
+    for(auto i:topNode) 
+    {
+        ans.push_back(i.second);
+    }
 
         return ans;
 }
