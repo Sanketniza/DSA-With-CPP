ab656536b3cb2c2b4f0127b8b9caa35019d341ba
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-04-06 10:14:13 +0530

    Add additional nodes to binary tree for boundary traversal testing

--- DSA With Sanket/Tree/Binary Tree/Questions Practice/20_Boundary.cpp
+++ DSA With Sanket/Tree/Binary Tree/Questions Practice/20_Boundary.cpp
@@ -108,8 +108,12 @@ int main() {
     root -> right = new Node(3);
     root -> left -> left = new Node(4);
     root -> left -> right = new Node(5);
+    root -> left -> left -> left = new Node(8);
+    root -> left -> left -> right = new Node(9);
     root -> right -> left = new Node(6);
     root -> right -> right = new Node(7);
+    root -> right -> right -> right = new Node(10);
+    root -> right -> right -> left = new Node(11);
 
     vector<int> ans = solve(root);
 
