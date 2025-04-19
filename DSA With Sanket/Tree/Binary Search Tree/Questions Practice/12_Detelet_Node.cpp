c1268476d77a1a9db76b97f32b52107b9ac55af2
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-04-19 19:57:02 +0530

    Refactor and update implementation to find the K-th smallest element in a binary search tree

--- DSA With Sanket/Tree/Binary Search Tree/Questions Practice/12_Detelet_Node.cpp
+++ DSA With Sanket/Tree/Binary Search Tree/Questions Practice/12_Detelet_Node.cpp
@@ -1,157 +1,122 @@
-/* 
-    ? lecture 70 : Partial BST
-    ? https://www.naukri.com/code360/problems/validate-bst_799483?leftPanelTab=0&leftPanelTabValue=PROBLEM
+/*
+    ? lecture 70:  Find K-th smallest Element in BST
+    ? https://www.naukri.com/code360/problems/find-k-th-smallest-element-in-bst_1069333?leftPanelTab=0&leftPanelTabValue=PROBLEM
 
     Problem statement
-Given a binary tree with N number of nodes, check if that input tree is Partial BST (Binary Search Tree) or not. If yes, return true, return false otherwise.
+    Given a binary search tree and an integer ‘K’. Your task is to find the ‘K-th’ smallest element in the given BST( binary search tree).
 
-A binary search tree (BST) is said to be a Partial BST if it follows the following properties.
+    BST ( binary search tree) -
 
-• The left subtree of a node contains only nodes with data less than and equal to the node’s data.
-• The right subtree of a node contains only nodes with data greater than and equal to the node’s data.
-• Both the left and right subtrees must also be partial binary search trees.
-Example:
-Input:
+    If all the smallest nodes on the left side and all the greater nodes on the right side of the node current node.
+
+    Example -
 
 
-Answer:
+    Order of elements in increasing order in the given BST is - { 2, 3, 4, 5, 6, 7, 8, 10 }
 
-Level 1: 
+    Suppose given ‘K = 3’ then 3rd smallest element is ‘4’.
 
-All the nodes in the left subtree of 4 (2, 1, 3) are smaller 
-than 4, all the nodes in the right subtree of the 4 (5) are 
-larger than 4.
+    Suppose given ‘K = 8’ then 8th smallest element is ‘10’.
+
+    Note:
+    1. You are not required to print the output explicitly, it has already been taken care of. Just implement the function and return the ‘K-th’ smallest element of BST.
+    2. You don’t need to return ‘K-th’ smallest node, return just value of that node.
+    3. If ‘K-th’ smallest element is not present in BST then return -1.
 
  */
 
-#include<iostream>
-#include<climits>
+#include <iostream>
 using namespace std;
 
-template <typename T>
-
-class Node {
-
+template <typename T>  // template for generic type , T can be int , char , float , double , etc.
+class bst
+{
     public:
-
         T data;
-        Node *left;
-        Node *right;
+        bst *left;
+        bst *right;
 
-        Node (int d) {
-            this -> data = d;
-            this -> left = NULL;
-            this -> right = NULL;
+        bst(T data)
+        {
+            this->data = data;
+            this->left = NULL;
+            this->right = NULL;
         }
-
 };
 
-bool isSolve(Node<int> *root , int min , int max) {
-
+int kthSmallest(bst<int> *root, int k , int &i)
+{
     // base case
     if(root == NULL) {
-        return true;
+        return -1;
     }
 
-    if(root -> data >= min && root -> data <= max) {
+    // recursive call
 
-        bool left = isSolve(root -> left , min , root -> data);
-        bool right = isSolve(root -> right , root -> data , max);
+    int left = kthSmallest(root -> left , k , i);
+    // cout << "left is : " << left << endl;
+
+    if(left != -1) {
+        // cout << "left is not -1" << endl;
+        return left;
     }
 
-    else {
-        return false;
+    // N
+    i++;
+
+    if( i == k ) {
+        return root -> data;
     }
 
-    // if(root == NULL) return true;
-
-    // if(root -> data <= min || root -> data >= max) return false;
-
-    // return isPartialBST(root -> left , min , root -> data) && isPartialBST(root -> right , root -> data , max);
+    //R
+    return kthSmallest(root -> right , k , i);
 
 }
 
-bool isPartialBST(Node<int> *root) {
+void solve(bst<int> *root , int k)
+{
+    static int count = 0; // static variable to keep track of the count of nodes visited
+    static int result = -1;
 
-    return isSolve(root , INT_MIN , INT_MAX);
-    
+    if(root == NULL) {
+        return;
+    }
+
+    // Inorder Traversal
+    solve(root -> left , k);
+
+    count++;
+
+    if(count == k) {
+        result = root -> data;
+        cout << "The " << k << " th smallest element in the BST is: " << result << endl;
+        return;
+    }
+
+    solve(root -> right , k);
 }
 
-int main() {
 
-    Node<int> *root = new Node<int>(4);
-    root -> left = new Node<int>(2);
-    root -> right = new Node<int>(5);
-    root -> left -> left = new Node<int>(1);
-    root -> left -> right = new Node<int>(3);
+int main()
+{
 
-    if(isPartialBST(root)) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
+    bst<int> *root = new bst<int>(15);
+    root->left = new bst<int>(10);
+    root->right = new bst<int>(20);
+    root->left->left = new bst<int>(8);
+    root->left->left->left = new bst<int>(5);
+    root->left->right = new bst<int>(12);
+    root->left->right->left = new bst<int>(11);
+    root->left->right->right = new bst<int>(13);
+
+    // inorder traversal : 5 8 10 11 12 13 15 20
+
+    int k = 4;
+    int i = 0;
+    cout << kthSmallest(root, k , i) << endl;
+
+    // second approach
+    solve(root , k);
 
     return 0;
-
 }
-
-/* 
-
-    ! imp theory
-
-   ^ BinaryTreeNode<int> *root , here why we use <int> 
-   ^ we can directly write like this BinaryTreeNode *root:
-   ^ also tell what is diff between them 
-
-       The use of BinaryTreeNode<int> *root versus BinaryTreeNode *root depends on whether the BinaryTreeNode class is defined as a template or not. Let's clarify the differences:
-    1. Template Class Definition:
-    If BinaryTreeNode is defined as a template class, it allows you to create nodes that can hold different data types. The syntax for defining a template class looks like this:
-    
-    
-  *  template <typename T>
-    class BinaryTreeNode {
-    public:
-
-        T data; // This can hold any data type specified when creating the node.
-        BinaryTreeNode<T> *left;
-        BinaryTreeNode<T> *right;
-    
-        BinaryTreeNode(T data) {
-            this->data = data;
-            left = NULL;
-            right = NULL;
-        }
-    };
-    
-    
-    2. Using the Template:
-    When you declare BinaryTreeNode<int> *root, you are specifying that this particular instance of BinaryTreeNode will hold integer values. This means that the data member of this node will be of type int.
-    3. Non-Template Class:
-    If you were to define BinaryTreeNode without templates, like this:
-    
-    
-  *  class BinaryTreeNode {
-    public:
-        int data; // Fixed to hold only integers.
-        BinaryTreeNode *left;
-        BinaryTreeNode *right;
-    
-    
-        BinaryTreeNode(int data) {
-            this->data = data;
-            left = NULL;
-            right = NULL;
-        }
-    };
-    
-    In this case, you would declare BinaryTreeNode *root without the <int>, because the class is not templated and is specifically designed to hold integers.
-    Differences:
-    Flexibility:
-    BinaryTreeNode<int> *root: This allows you to create nodes that can hold integers. You can also create nodes for other data types (e.g., BinaryTreeNode<double>, BinaryTreeNode<string>, etc.) by specifying a different type.
-    BinaryTreeNode *root: This would only work if BinaryTreeNode is defined to hold a specific type (like int), and you cannot change the type without modifying the class definition.
-    Type Safety:
-    Using templates provides type safety, ensuring that the data type is consistent throughout the tree. If you try to insert a different type into a BinaryTreeNode<int>, the compiler will give you an error.
-    Summary:
-    Use BinaryTreeNode<int> *root if BinaryTreeNode is a template class that can hold various data types.
-    Use BinaryTreeNode *root if BinaryTreeNode is a non-template class that is fixed to a specific data type (like int).
- */
