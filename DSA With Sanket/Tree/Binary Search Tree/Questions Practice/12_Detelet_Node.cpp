327f8103e8fa9f8a8dab152b8d8c841d48fcdb2b
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-04-21 19:50:44 +0530

    Update implementation to search for a node in a binary search tree, replacing the previous LCA functionality. Add both recursive and iterative methods for searching.

--- DSA With Sanket/Tree/Binary Search Tree/Questions Practice/12_Detelet_Node.cpp
+++ DSA With Sanket/Tree/Binary Search Tree/Questions Practice/12_Detelet_Node.cpp
@@ -1,146 +1,129 @@
-
 /* 
-    ? lecture 70:  LCA of Two Nodes In A BST
-    ? https://www.naukri.com/code360/problems/lca-in-a-bst_981280?leftPanelTab=0&leftPanelTabValue=PROBLEM
+    ? lecture 69 : Search in BST
+    ? https://www.naukri.com/code360/problems/search-in-bst_1402878?count=25&page=2&search=&sort_entity=order&sort_order=ASC&attempt_status=NOT_ATTEMPTED&leftPanelTabValue=PROBLEM
 
     Problem statement
-    You are given a binary search tree of integers with N nodes. You are also   given references to two nodes 'P' and 'Q' from this BST.
-
-
-
-    Your task is to find the lowest common ancestor(LCA) of these two given     nodes.
-
-
-
-    The lowest common ancestor for two nodes P and Q is defined as the lowest   node that has both P and Q as descendants (where we allow a node to be a  descendant of itself)
-
-
-
-    A binary search tree (BST) is a binary tree data structure which has the    following properties.
-
-    • The left subtree of a node contains only nodes with data less than the    node’s data.
-    • The right subtree of a node contains only nodes with data greater than    the node’s data.
-    • Both the left and right subtrees must also be binary search trees.
-
-
-    For example:
-    'P' = 1, 'Q' = 3
-    tree = 2 1 4 -1 -1 3 -1 -1 -1,
-
-    The BST corresponding will be- 
-
-    Here, we can clearly see that LCA of node 1 and node 3 is 2.
+    There is a Binary Search Tree (BST) consisting of ‘N’ nodes. Each node of     this BST has some integer data.
+    
+    
+    
+    You are given the root node of this BST, and an integer ‘X’. Return true     if there is a node in BST having data equal to ‘X’, otherwise return false.
+    
+    
+    
+    A binary search tree (BST) is a binary tree data structure that has the     following properties:
+    
+    1. The left subtree of a node contains only nodes with data less than the     node’s data.
+    
+    2. The right subtree of a node contains only nodes with data greater than     the node’s data.
+    
+    3. The left and right subtrees must also be binary search trees.
+    Note:
+    It is guaranteed that all nodes have distinct data.
     Detailed explanation ( Input/output format, Notes, Images )
-    Sample Input 1 :
-    3 5
-    2 1 3 -1 -1 -1 5 -1 -1
+    Sample Input 1:
+    7 8
+    4 2 6 1 3 5 7 -1 -1 -1 -1 -1 -1 -1 -1
     Sample Output 1:
-    3
-    Explanation for Sample 1:
-    The BST corresponding will be-
-
-    Here, we can clearly see that LCA of node 3 and node 5 is 3.
-    Sample Input 2 :
-    1 2
-    3 2 -1 1 -1 -1 -1
+    False
+    Explanation For Sample Input 1:
+    There is no node having data 8. See the problem statement for the picture     of this BST.
+    Sample Input 2:
+    4 1
+    3 1 5 -1 2 -1 -1 -1 -1
     Sample Output 2:
-    2
-    Constraints:
-    1 <= 'N' <= 10^5
-    1 <= Data <= 10^6
-
-    Time Limit: 1sec
-
+    True
+    Explanation For Sample Input 1:
+    There is a left node, and it has data 1. Thus, we should print ‘True’. 
  */
 
+
+
+ // todo: Here are two ways to solve this problem
+
+ //* 1. Recursive way
+    //^ Time Complexity : O(N)
+    //^ Space Complexity : O(N)
+
+ //* 2. Iterative way
+    //^ Time Complexity : O(N)
+    //^ Space Complexity : O(1)
+
+
 #include<iostream>
+#include<vector>
 using namespace std;
 
- /* 
-    ? Two ways to solve this problem
-
-        * Recursive way
-            ^ - Time Complexity = O(n);
-            ^ - space Complexity = O(n);
-
-        * Iterative way
-            ^ - Time Complexity = O(n);    
-            ^ - Time Complexity = O(1);     
- 
-  */
-
-class  bst {
+class Node {
     public:
-        int data;
-        bst *left;
-        bst *right;
+    int data;
+    Node *left;
+    Node *right;
 
-        bst(int data) {
-            this -> data = data;
-            this -> left = NULL;
-            this -> right = NULL;
-        }
+    Node(int data) {
+        this -> data = data;
+        this -> left = NULL;
+        this -> right = NULL;
+    }
 };
 
-//todo: Recursive way
-
-bst * lcaOfBST(bst *root , int p , int q) {
+ //* 1. Recursive way
+bool SearchInBST(Node *root , int x) {
 
     if(root == NULL) {
-        return NULL;
+        return false;
     }
 
-    if(root -> data > p && root -> data > q) {
-        return lcaOfBST(root -> left , p , q );
+    if (root -> data == x) {
+        return true;
     }
 
-    if(root -> data < p && root -> data < q) {
-        return lcaOfBST (root -> right , p , q);
+    // recursive call
+    if( root -> data > x) {
+        return SearchInBST(root -> left , x);
+    } 
+    
+    else {
+        return SearchInBST(root -> right , x);
     }
-
-    return root;
-
 }
 
-//todo: Iterative way
-bst * lcaOfBST_Iterative(bst *root , int p , int q) {
-
-    while (root != NULL) {
-
-        if(root -> data > p && root -> data > q) {
-             root = lcaOfBST_Iterative(root -> left , p , q );
+ //* 2. Iterative way
+bool Iterativeway(Node* root, int x) {
+    Node* current = root;
+    
+    while (current != NULL) {
+        if (current->data == x) {
+            return true;
         }
-
-        else if (root -> data < p && root -> data < q) {
-             root = lcaOfBST_Iterative (root -> right , p , q);
+        else if (x < current->data) {
+            current = current->left;
         }
-
         else {
-            return root;
+            current = current->right;
         }
     }
+    return false;
 }
- 
 
 int main() {
 
-    
-    bst *root = new bst(15);
-    root -> left = new bst(10);
-    root -> right = new bst(20);
-    root -> left -> left = new bst(8);
-    root -> left -> right = new bst(12);
-    root -> right -> left = new bst(16);
-    root -> right -> right = new bst(25);
+    Node *root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(6);
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    root->right->left = new Node(5);
+    root->right->right = new Node(7);
 
-    int p = 10;
-    int q = 25;
+    int x = 25;
 
-    bst *ans = lcaOfBST(root , p , q);
-    cout << "Recursive way: " << ans -> data << endl;
+    //* 1. Recursive way
+    cout << "Recursive Search in BST : " << SearchInBST(root, x) << endl;
 
-    bst *ans2 = lcaOfBST_Iterative(root , p , q);
-    cout << "Iterative way: " << ans2 -> data << endl;
+    //* 2. Iterative way
+    bool result = Iterativeway(root, x);
+    cout << "Iterative Search in BST : " << (result ? "True" : "False") << endl;
 
- return 0;
+    return 0;
 }
