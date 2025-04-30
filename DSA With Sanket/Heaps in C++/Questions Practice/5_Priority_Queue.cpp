aa922924ce2928002974fe988741fef7754fa35b
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-04-30 12:56:05 +0530

    Add functionality to demonstrate min-heap operations in priority queue implementation. Included size checks, top element retrieval, and element iteration for better understanding of min-heap behavior.

--- DSA With Sanket/Heaps in C++/Questions Practice/5_Priority_Queue.cpp
+++ DSA With Sanket/Heaps in C++/Questions Practice/5_Priority_Queue.cpp
@@ -24,6 +24,30 @@ int main() {
         cout << max.top() << " ";
         max.pop();
     }
-    
+
+    cout << endl;
+
+    priority_queue<int , vector<int> , greater<int> > min;
+    min.push(2);
+    min.push(3);
+    min.push(1);
+    min.push(34);
+    min.push(4);
+
+    cout << min.size() << endl;
+    cout << min.top() << endl;
+    min.pop();
+
+    cout << min.top() << endl;
+    cout << min.size() << endl;
+
+    cout << "elements of priority queue are : " << endl;
+    while(!min.empty()) {
+        cout << min.top() << " ";
+        min.pop();
+    }
+
+    cout << endl;
+
  return 0;
 }
