69ea9a0e7c567a5e20b6ed5ef17ebf6db38fa80e
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-05-01 11:52:59 +0530

    Fix heap merging logic to use min-heap instead of max-heap. Update example vectors and add heap construction for correct output.

--- DSA With Sanket/Heaps in C++/Questions Practice/10_Merge_Heap.cpp
+++ DSA With Sanket/Heaps in C++/Questions Practice/10_Merge_Heap.cpp
@@ -2,10 +2,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void heap(vector<int> &v , int n , int i) {
+    
+    int parent = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if(left < n && v[left] > v[parent]) {
+        parent = left;
+    }
+
+    if(right < n && v[right] > v[parent]) {
+        parent = right;
+    }
+
+    if(parent != i) {
+        swap(v[i], v[parent]);
+        heap(v, n, parent);
+    }
+}
+
 int main() {
 
-    vector<int> a = {10, 5, 6, 2};
-    vector<int> b = {12, 7, 9};
+    vector<int> a = {1, 2, 3, 4, 5, 6};
+    vector<int> b = {1, 2, 3, 4};
 
     priority_queue<int> q;
 
@@ -28,7 +48,36 @@ int main() {
         cout << ans[i] << " ";
     }
     cout << endl;
+
     cout << "Size of Merged Heap: " << ans.size() << endl;
     cout << "Max Element: " << ans[0] << endl;
+
+    // above code is not correct it will give wrong output
+
+    // because we are using max heap to merge two heaps
+    // so we need to use min heap to merge two heaps
+
+    vector<int> v;
+    for(auto i : a) {
+        v.push_back(i);
+    }
+
+    for(auto i : b) {
+        v.push_back(i);
+    }
+
+    int n = v.size();
+
+    for(int i = n/2 - 1; i >=0; i-- ) {
+        heap(v , n  , i);
+    }
+
+    cout << "element in heap are " << endl;
+    for(auto i : v) {
+        cout << i << " ";
+    }
+        cout << endl;
+
+
  return 0;
 }
