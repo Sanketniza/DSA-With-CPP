dad05ee3939e3352a70356781b849df941eef71d
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-05-11 18:01:13 +0530

    Add implementation for shortest path in directed acyclic graphs and adjacency list printing

--- DSA With Sanket/Graph/Questions Practice/18_Shortest_path_Acycle.cpp
+++ DSA With Sanket/Graph/Questions Practice/18_Shortest_path_Acycle.cpp
@@ -1,59 +1,50 @@
-/* 
-
-    ? Shortest path in a directed acyclic graph
-    ?https://www.naukri.com/code360/library/shortest-path-in-a-directed-acyclic-graph
-
-    
-
- */
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
- 
-class Graph{
 
+class Graph {
     public:
 
-        unordered_map<int , list<pair<int , int> > > adj;
+        unordered_map<int , list <pair<int , int >>> adj;
 
         void addEdge(int u , int v , int wt) {
             pair<int , int> p = make_pair(v , wt);
             adj[u].push_back(p);
         }
 
-        void PrintAdj() {
+        void printAdj() {
+
             cout << "Adjacency List of the Graph : " << endl;
-            for(auto i:adj) {
-
+            for(auto i : adj) {
                 cout << i.first << " -> ";
-
-                for(auto j:i.second) {
+                for(auto j : i.second) {
                     cout << "(" << j.first << " , " << j.second << ") ";
                 }
                 cout << endl;
             }
+
         }
 
-        void dfs(int node , unordered_map<int, bool> &vis , stack<int> &st) {
+        void topoSort(int node , unordered_map<int, bool> &vis , stack<int> &st) {
             vis[node] = true;
-            for(auto i:adj[node]) {
+
+            for(auto i : adj[node]) {
                 if(!vis[i.first]) {
-                    dfs(i.first , vis , st);
+                    topoSort(i.first , vis , st);
                 }
             }
-            st.push(node);
         }
 
         void shortestPath(int src , vector<int> &dist , stack<int> &st) {
 
             dist[src] = 0;
 
-            while (!st.empty()) {
+            while(!st.empty()) {
                 int top = st.top();
                 st.pop();
 
                 if(dist[top] != INT_MAX) {
-                    for(auto i:adj[top]) {
+                    for(auto i : adj[top]) {
                         if(dist[top] + i.second < dist[i.first]) {
                             dist[i.first] = dist[top] + i.second;
                         }
@@ -61,9 +52,10 @@ class Graph{
                 }
             }
         }
-  
-}; 
-int main() { 
+
+};
+
+int main() {
 
     Graph g;
 
@@ -78,72 +70,30 @@ int main() {
     g.addEdge(3 , 4 , -1);
     g.addEdge(4 , 5 , -2);
 
-    /*  
-
-        0
-       / \
-      1   2
-      |  /|\
-      3 | | 5
-       \|/
-        4
-
-
-        
-
-        0 -> 1 -> 3
-        0 -> 2 -> 5
-        0 -> 2 -> 4
-        1 -> 2 -> 4
-        1 -> 2 -> 5
-        2 -> 3
-        2 -> 4
-        3 -> 4
-        4 -> 5
-        2 -> 5
-        
-
-    
-     */
-
-
-    g.PrintAdj(); // for checking the adjacency list
-    // we need to do the topological sort
-
-    // topology sort
+    //* topological sort
 
     int n = 6;
-
     stack<int> st;
     unordered_map<int, bool> vis;
     for(int i = 0; i < n; i++) {
         if(!vis[i]) {
-            g.dfs(i , vis , st);
+            g.topoSort(i , vis , st);
         }
     }
 
-   
     int src = 1;
     vector<int> dist(n);
-       
-
-    for(int i = 0; i < n; i++) {
+    for(auto i: dist) {
         dist[i] = INT_MAX;
     }
 
-    // dist[src] = 0;
+    g.shortestPath(src , dist , st);
 
-    g.shortestPath(src, dist , st);
-
-
-
-    cout << "ans is : ";
-
-    for(auto i:dist) {
-        cout << i << " ";
+    cout << "Shortest Path from source node " << src << " : " << endl;
+    for(int i = 0; i < n; i++) {
+        cout << "Node " << i << " : " << dist[i] << endl;
     }
 
-    cout << endl;
 
-    return 0;
+ return 0;
 }
