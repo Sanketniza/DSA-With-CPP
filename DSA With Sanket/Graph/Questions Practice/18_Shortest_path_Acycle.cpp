afbaafe54cb919826af4641e5c060d7f46a95658
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-05-18 22:39:46 +0530

    Implement BFS-based shortest path algorithm for undirected graphs

--- DSA With Sanket/Graph/Questions Practice/18_Shortest_path_Acycle.cpp
+++ DSA With Sanket/Graph/Questions Practice/18_Shortest_path_Acycle.cpp
@@ -1,99 +1,104 @@
-#include<iostream>
-#include<bits/stdc++.h>
+/* 
+   ? Shortest Path in Undirected Graphs
+   ? https://www.youtube.com/watch?v=abIEXKFpLNE&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=102
+
+   Problem statement
+The city of Ninjaland is analogous to the unweighted graph. The city has ‘N’ houses numbered from 1 to ‘N’ respectively and are connected by M bidirectional roads. If a road is connecting two houses ‘X’ and ‘Y’ which means you can go from ‘X’ to ‘Y’ or ‘Y’ to ‘X’. It is guaranteed that you can reach any house from any other house via some combination of roads. Two houses are directly connected by at max one road.
+
+A path between house ‘S’ to house ‘T’ is defined as a sequence of vertices from ‘S’ to ‘T’. Where starting house is ‘S’ and the ending house is ‘T’ and there is a road connecting two consecutive houses. Basically, the path looks like this: (S , h1 , h2 , h3 , ... T). you have to find the shortest path from ‘S’ to ‘T’.
+
+For example
+In the below map of Ninjaland let say you want to go from S=1 to T=8, the shortest path is (1, 3, 8). You can also go from S=1 to T=8  via (1, 2, 5, 8)  or (1, 4, 6, 7, 8) but these paths are not shortest.
+ */
+
+#include <bits/stdc++.h>
 using namespace std;
 
-class Graph {
-    public:
+vector<int> solve (vector<pair<int, int> > &edges , int n , int s , int t ) {
 
-        unordered_map<int , list <pair<int , int >>> adj;
+   unordered_map<int , list<int> > adj;
+	for(int i = 0; i < edges.size(); i++) {
+		int u = edges[i].first;
+		int v = edges[i].second;
 
-        void addEdge(int u , int v , int wt) {
-            pair<int , int> p = make_pair(v , wt);
-            adj[u].push_back(p);
-        }
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
 
-        void printAdj() {
+	// know do bfs for parent
 
-            cout << "Adjacency List of the Graph : " << endl;
-            for(auto i : adj) {
-                cout << i.first << " -> ";
-                for(auto j : i.second) {
-                    cout << "(" << j.first << " , " << j.second << ") ";
-                }
-                cout << endl;
-            }
+	unordered_map<int , bool > visited;
+	unordered_map<int , int> parent;
+	queue<int> q;
 
-        }
+	q.push(s);
+	visited[s] = true;
+	parent[s] = -1;
 
-        void topoSort(int node , unordered_map<int, bool> &vis , stack<int> &st) {
-            vis[node] = true;
+	while (!q.empty()) {
+		int front = q.front();
+		q.pop();
 
-            for(auto i : adj[node]) {
-                if(!vis[i.first]) {
-                    topoSort(i.first , vis , st);
-                }
-            }
-        }
+		for(auto i:adj[front]) {
+			if(!visited[i]) {
+				q.push(i);
+				parent[i] = front;
+				visited[i] = true;
+			}
+		}
+	}
 
-        void shortestPath(int src , vector<int> &dist , stack<int> &st) {
+	// shortest path
 
-            dist[src] = 0;
+	vector<int> ans;
+	int currentNode = t;
+	ans.push_back(t);
 
-            while(!st.empty()) {
-                int top = st.top();
-                st.pop();
+	while (currentNode != s) {
+		currentNode = parent[currentNode];
+		ans.push_back(currentNode);
+	}
 
-                if(dist[top] != INT_MAX) {
-                    for(auto i : adj[top]) {
-                        if(dist[top] + i.second < dist[i.first]) {
-                            dist[i.first] = dist[top] + i.second;
-                        }
-                    }
-                }
-            }
-        }
+	// reverse the ans
 
-};
+	reverse(ans.begin() , ans.end());
 
-int main() {
+	return ans;
 
-    Graph g;
+}
 
-    g.addEdge(0 , 1 , 5);
-    //* (u <node1> , v <node2>, weight)
-    g.addEdge(0 , 2 , 3);
-    g.addEdge(1 , 2 , 2);
-    g.addEdge(1 , 3 , 6);
-    g.addEdge(2 , 3 , 7);
-    g.addEdge(2 , 4 , 4);
-    g.addEdge(2 , 5, 2);
-    g.addEdge(3 , 4 , -1);
-    g.addEdge(4 , 5 , -2);
+int main()
+{
+    /* 
+            2 -- -- -----5
+        /                  \
+      1 - - - 3 - - -   - -  8
+        \                  /
+          4 -- 6 - - - -7
+    
+     */
 
-    //* topological sort
+    vector<pair<int, int> > edges = {
+         {1, 2}, {1, 3}, {1, 4},
+         {2, 5}, {3, 5}, {3, 6},
+         {4, 6}, {5, 8}, {6, 7},
+         {7, 8}
+    };
 
-    int n = 6;
-    stack<int> st;
-    unordered_map<int, bool> vis;
-    for(int i = 0; i < n; i++) {
-        if(!vis[i]) {
-            g.topoSort(i , vis , st);
-        }
+    int n = 8;
+    int source = 1;
+    int dest = 8;
+    int edge = 8;
+
+    vector<int> result = solve(edges, n ,source, dest );
+    
+    cout << "path is : " ;
+
+    for(int i = 0; i < result.size() - 1; i++) {
+        cout << result[i] << " -> ";
     }
+    
+    cout << result.back();
 
-    int src = 1;
-    vector<int> dist(n);
-    for(auto i: dist) {
-        dist[i] = INT_MAX;
-    }
-
-    g.shortestPath(src , dist , st);
-
-    cout << "Shortest Path from source node " << src << " : " << endl;
-    for(int i = 0; i < n; i++) {
-        cout << "Node " << i << " : " << dist[i] << endl;
-    }
-
-
- return 0;
+    return 0;
 }
