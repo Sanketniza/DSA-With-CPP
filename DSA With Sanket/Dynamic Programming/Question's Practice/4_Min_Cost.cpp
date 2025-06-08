00579aa6cf69ba883597ae48cea49bf34f43b049
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-06-08 12:19:21 +0530

    Add bottom-up dynamic programming solution for minimum cost climbing stairs; include tabulation approach

--- DSA With Sanket/Dynamic Programming/Question's Practice/4_Min_Cost.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/4_Min_Cost.cpp
@@ -27,6 +27,20 @@ int solve1(vector<int> &cost , int n , vector<int> &dp) {
     // return dp[n] = ans; // This line can also be used directly
 }
 
+int solve2(vector<int> &cost , int n) {
+
+   vector<int> dp(n + 1);
+
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i = 2; i < n; i++) {
+        dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+    }
+
+    return min(dp[n - 1], dp[n - 2]);
+}
+
 int main() {
 
     int m;
@@ -49,6 +63,10 @@ int main() {
     vector<int> dp(n + 1 , -1);
     int b = min(solve1(cost , n - 1 , dp) , solve1(cost , n - 2 , dp));
     cout << "ans is : " << b << endl;
+
+    // Approach 2: Bottom-Up (Tabulation)
+    int c = solve2(cost, n );
+    cout << "ans2 is : " << c << endl;
 
  return 0;
 }
