0569719bd5d1b2113bcb25b4b30f4f1878b71bb3
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-06-08 19:10:19 +0530

    Add memoization solution for minimum number of coins; include implementation and input handling

--- DSA With Sanket/Dynamic Programming/Question's Practice/5_Min_No_Coin.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/5_Min_No_Coin.cpp
@@ -24,6 +24,31 @@ int solve(vector<int> cost, int sum) {
     return mini;
 }
 
+int Memoization(vector<int> cost , int sum , vector<int> &dp) {
+    
+    // base case
+    if(sum == 0) return 0;
+
+    if(sum < 0 ) return INT_MAX;
+
+    if(dp[sum] != -1) return dp[sum];
+
+    int mini = INT_MAX;
+
+    for(int i = 0; i < cost.size(); i++) {
+        
+        int ans = Memoization(cost, sum - cost[i], dp);
+
+        if(ans != INT_MAX) {
+            mini = min(mini , 1 + ans);
+        }
+    }
+
+    dp[sum] = mini;
+    // return dp[sum];
+    return mini;
+}
+
 int main() {
 
     vector<int> cost = {10, 15, 20, 25, 30};
@@ -36,7 +61,15 @@ int main() {
     //     cout << "Minimum number of coins required: " << ans << endl;
     // }
 
-    
+    //* dp
+    vector<int> dp(sum + 1, -1);
+    int ans = Memoization(cost, sum , dp);
+
+    if(ans == INT_MAX) {
+        cout << "Not Possible" << endl;
+    } else {
+        cout << "Minimum number of coins required: " << ans << endl;
+    }
 
  return 0;
 }
