a9c1c094c904335a8a68c8fdee2eaf2a16c2274a
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-06-09 12:01:05 +0530

    Add dynamic programming solutions for cutting a rod; implement recursive and tabulation approaches

--- DSA With Sanket/Dynamic Programming/Question's Practice/7_cut_rod.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/7_cut_rod.cpp
@@ -17,6 +17,47 @@ int solve(int n , int x , int y , int z) {
     return ans;
 }
 
+int d(int n , int x , int y , int z , vector<int> &dp) {
+
+    // base case
+    if(n == 0) return 0;
+    if(n < 0) return INT_MIN;
+
+    // check if already computed
+    if(dp[n] != -1) return dp[n];
+
+    // recursive call
+    int a = d(n - x, x, y, z, dp) + 1;
+    int b = d(n - y, x, y, z, dp) + 1;
+    int c = d(n - z, x, y, z, dp) + 1;
+
+    int ans = max(a, max(b, c));
+
+    return  dp[n] = ans; // store the result in dp array
+ 
+}
+
+int SolveTab(int n , int x , int y , int z) {
+
+    vector<int> dp(n + 1 , INT_MIN); //if(n < 0) return INT_MIN;
+    dp[0] = 0; // base case if(n == 0) return 0;
+
+    for(int i = 1; i <= n; i++) {
+
+        if(i - x >= 0)
+            dp[i] = max(dp[i] , dp[i - x] + 1);
+
+        if(i - y >= 0)
+            dp[i] = max(dp[i] , dp[i - y] + 1);
+
+        if(i - z >= 0)
+            dp[i] = max(dp[i] , dp[i - z] + 1);
+
+    }
+
+    return  dp[n];
+}
+
 int main() {
 
     int n = 7; // Length of the rod
@@ -31,6 +72,23 @@ int main() {
         cout << "No valid segments can be cut." << endl;
     } else {
         cout << "Maximum number of segments that can be cut: " << ans << endl;
+    }
+
+    // dp
+    vector<int> dp(n + 1, -1);
+    int dpAns = d(n , x , y , z , dp);
+    if(dpAns < 0) {
+        cout << "No valid segments can be cut using DP." << endl;
+    } else {
+        cout << "Maximum number of segments that can be cut using DP: " << dpAns << endl;
+    }
+
+    // tabulation way
+    int tabAns = SolveTab(n , x , y , z);
+    if(tabAns < 0) {
+        cout << "No valid segments can be cut using Tabulation." << endl;
+    } else {
+        cout << "Maximum number of segments that can be cut using Tabulation: " << tabAns << endl;
     }
 
  return 0;
