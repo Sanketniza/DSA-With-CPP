8f7c28a7c0a508322bd6deed15896b7b0fc3ad96
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-06-09 19:04:19 +0530

    Add memoization and tabulation solutions for counting derangements; implement logic and update main function outputs

--- DSA With Sanket/Dynamic Programming/Question's Practice/9_Count_Derangement.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/9_Count_Derangement.cpp
@@ -17,6 +17,34 @@ int solve( int n) {
     
 }
 
+int mamo(int n , vector<int> &dp) {
+
+    // base case
+    if(n == 0) return 0;
+    if(n == 1) return 0;
+    if(n == 2) return 1;
+
+    if(dp[n] != -1) return dp[n];
+
+    int ans = (n - 1) * (mamo(n - 1, dp) + mamo(n - 2, dp));
+
+    return dp[n] = ans;
+}
+
+int tab(int n) {
+
+    vector<int> dp(n + 1, 0);
+    dp[0] = 0; // base case
+    dp[1] = 0; // base case
+    dp[2] = 1; // base case
+
+    for(int i = 3; i <= n; i++) {
+       dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]);
+    }
+
+    return dp[n];
+}
+
 int main() {
 
     int n = 3;
@@ -24,6 +52,15 @@ int main() {
     // first way 
     int a = solve(n);
     cout << "Count of derangements using recursion: " << a << endl;
+
+    // second way
+    vector<int> dp(n + 1, -1);
+    int b = mamo(n , dp);
+    cout << "Count of derangements using memoization: " << b << endl;
+
+    // third way
+    int c = tab(n);
+    cout << "Count of derangements using tabulation: " << c << endl;
 
  return 0;
 }
