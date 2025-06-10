87622e8e7bfeca1f3582e2542302777b2627060d
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-06-10 18:29:50 +0530

    Add memoization solution for 0/1 Knapsack problem; implement newsolve function and update main function outputs

--- DSA With Sanket/Dynamic Programming/Question's Practice/11_01_KnapSack.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/11_01_KnapSack.cpp
@@ -34,6 +34,31 @@ int solve(vector<int> weighs , vector<int> value , int index , int capacity) {
     return ans;
 }
 
+int newsolve(vector<int> weighs , vector<int> value , int index , int capacity) {
+
+   // base case
+    if(index == weighs.size() - 1) {
+        if(weighs[index] <= capacity) {
+            return value[index];
+        }
+
+        else {
+            return 0;
+        }
+    }
+
+    int include = 0;
+    if(weighs[index] <= capacity) {
+        include = value[index] + newsolve(weighs , value , index + 1, capacity - weighs[index]);
+    }
+
+    int exclude = 0 + newsolve(weighs , value , index + 1, capacity);
+
+    int ans = max(include , exclude);
+
+    return ans;
+}
+
 int main() {
 
     vector<int> weight = {1, 2, 3};
@@ -45,6 +70,9 @@ int main() {
     // Normal Recursion
     int a = solve(weight, value, n - 1, capacity);
     cout << "Maximum value (Recursion): " << a << endl;
+
+    int b = newsolve(weight, value, 0, capacity);
+    cout << "Maximum value (Memoization): " << b << endl;
 
  return 0;
 }
