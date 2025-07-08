054b952020c78f4db112b77d7b2017f415c310b3
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-07-08 09:56:46 +0530

    Add memoization and tabulation solutions for Count Derangements problem; implement solveMem and solveTab functions, and update main function to demonstrate both approaches.

--- DSA With Sanket/Dynamic Programming/Question's Practice/15_Count_derangement.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/15_Count_derangement.cpp
@@ -11,6 +11,35 @@ int solveRec(int n ) {
     return ans;
 }
 
+int solveMem(int n , vector<int> &dp) {
+
+    // base case
+    if( n == 1 ) return 0;
+    if( n == 2 ) return 1;
+
+    if(dp[n] != -1) return dp[n];
+
+    int ans =  (n - 1) * (solveMem(n - 1 , dp) + solveMem(n - 2 , dp));
+    dp[n] = ans;
+    return dp[n];
+}
+
+int solveTab(int n ) {
+
+    // create vector
+    vector<int> dp(n + 1 , 0);
+    dp[1] = 0;
+    dp[2] = 1;
+
+    for(int i = 3; i <= n; i++) {
+        int ans = (i - 1) * (dp[i - 2] + dp[i - 1]);
+        dp[i] = ans;
+    }
+
+    return dp[n];
+}
+
+
 int main() {
 
     int n = 3;
@@ -20,8 +49,32 @@ int main() {
     if(ans < 0) {
         cout << "Not Possible" << endl;
     }
+
     else {
         cout << ans << endl;
+    }
+
+    // using memoization
+    vector<int> dp(n + 1, -1);
+    int a = solveMem(n , dp);
+
+    if(a < 0) {
+        cout << "Not Possible" << endl;
+    }
+
+    else {
+        cout << a << endl;
+    }
+
+    // using tabulation
+    int b = solveTab(n);
+
+    if(b < 0) {
+        cout << "Not Possible" << endl;
+    }
+
+    else {
+        cout << b << endl;
     }
  return 0;
 }
