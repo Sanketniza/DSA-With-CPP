9cca925c75f6a728d44b52ddde02d0ad9dd4e431
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-07-08 09:23:05 +0530

    Update problem statements and links for Cut Rod into Segments and Minimum Cost for Tickets; improve formatting and add additional resources

--- DSA With Sanket/Dynamic Programming/Question's/113_Minimum_Cost_for_Tickets.cpp
+++ DSA With Sanket/Dynamic Programming/Question's/113_Minimum_Cost_for_Tickets.cpp
@@ -1,34 +1,36 @@
 /* 
-    113: Minimum Cost for Tickets
-    https://www.naukri.com/code360/problems/minimum-coins_2180776?leftPanelTab=0
+    ? 113: Minimum Cost for Tickets
+    ? https://www.naukri.com/code360/problems/minimum-coins_2180776?leftPanelTab=0
+    ? https://www.youtube.com/watch?v=1b2d3g0k4e5&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=113
 
      Ninja's Trip
 
-    Problem statement
     Ninja is willing to take some time off from his training and planning a year-long tour. You are given a DAYS array     consisting of ‘N’ days when Ninjas will be traveling during the year. Each Day is an integer between 1 to 365 (both     inclusive). Train tickets are sold in three different ways:
+
     A 1-day pass is sold for 'COST'[0] coins,
     A 7-day pass is sold for 'COST'[1] coins, and
     A 30-day  pass is sold for 'COST'[2] coins.
+
     The passes allow for many days of consecutive travel.
     Your task is to help the Ninja to find the minimum number of coins required to complete his tour. For example,
     If Ninja gets a 7-day pass on day 2, then he can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
     Detailed explanation ( Input/output format, Notes, Images )
-    Constraints:
-    1 <= T <= 10
-    1 <= N <= 365
-    1 <= DAYS[i] <= 365
+   
     
     Time Limit: 1 sec
     Sample Input 1:
-    2
+    
     2 
     2 5
     1 4 25    
+    
     7
     1 3 4 5 7 8 10
     2 7 20
+
     Sample Output 1:
     2
+    
     11
     Explanation For sample input 1:
     For the first test case, 
