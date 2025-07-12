48976e0904dc45137e3f2edb38f0311e5a1e497f
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-07-12 12:50:40 +0530

    Add memoization approach for Number of Dice problem; implement solveM function and update main function to demonstrate both recursive and memoized solutions.

--- DSA With Sanket/Dynamic Programming/Question's Practice/20_Number_Of_Dice.cpp
+++ DSA With Sanket/Dynamic Programming/Question's Practice/20_Number_Of_Dice.cpp
@@ -23,6 +23,31 @@ int solve(int dice ,int face , int target) {
     return ans;
 }
 
+int solveM(int dice ,int face ,int target , vector<vector<int>> &dp) {
+
+    // base case
+    if(target < 0) return 0;
+
+    if(target == 0 && dice == 0) return 1;
+
+    if(target == 0 && dice != 0) return 0;
+
+    if(target != 0 && dice == 0 ) return 0;
+
+    if(dp[dice][target] != -1) return dp[dice][target];
+
+    int ans = 0;
+
+    for(int i = 1; i <= face; i++) {
+
+        ans += solveM(dice - 1 , face , target - i , dp);
+    }
+
+    dp[dice][target] = ans;
+
+    return dp[dice][target];
+}
+
 int main() {
 
     int m = 6; int n = 3; int x = 12;
@@ -30,9 +55,12 @@ int main() {
     int ans = solve(n , m , x);
     cout << "ans is : " << ans << endl;
 
-    
+    // todo: Memoization approach
+    vector<vector<int>> dp(n + 1 , vector<int> (x + 1 , -1));
+    int a = solveM(n , m , x , dp);
+    cout << "ans is : " << a << endl;
      
      
 
- return 0;
+    return 0;
 }
