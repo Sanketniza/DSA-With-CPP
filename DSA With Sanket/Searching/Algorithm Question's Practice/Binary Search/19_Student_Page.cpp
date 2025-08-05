61da61bad20eb373fc2668aac6c70ab1cf2cf410
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-05 23:30:10 +0530

    Add binary search allocation algorithm: implement student page allocation logic in 19_Student_Page.cpp, enhancing the binary search approach for optimal page distribution.

--- DSA With Sanket/Searching/Algorithm Question's Practice/Binary Search/19_Student_Page.cpp
+++ DSA With Sanket/Searching/Algorithm Question's Practice/Binary Search/19_Student_Page.cpp
@@ -2,8 +2,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPossible(int arr[] , int n , int student , int mid) {
+
+    studentCount = 1;
+    pageSum = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        if(pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+
+        else {
+            studentCount++;
+
+            if(studentCount < student || arr[i] > mid) {
+                return false;
+            }
+
+            pageSum = arr[i];
+        }
+    }
+}
+
+int Allocation(int arr[] , int n , int student) {
+
+    int start = 0;
+    int sum = 0;
+
+    int ans = -1;
+
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    int end = sum;
+    int mid = start + (end - start) / 2;
+
+    while(start <= end) {
+
+        if(isPossible(arr , n , student , mid)) {
+            ans = mid;
+            end = mid - 1;
+        }
+
+        else {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+
+    return ans;
+}
+
 int main() {
 
-    
+    int arr[] = {10,20,30,40};
+    int n = 4;
+    int studnet = 2;
+
+    int result = Allocation(arr , n , student);
  return 0;
 }
