7a546dce03812650e1034c07f60079ccbf26b865
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-08 22:30:34 +0530

    Refactor Trie implementation: streamline search methods and enhance output formatting in 5_Trie_Imp.cpp for improved usability and clarity.

---
+++ DSA With Sanket/Stack/Stack Question Practice/18_Invalid_P.cpp
@@ -0,0 +1,58 @@
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+
+  string s = "[()]{}{[()()]()}";
+  stack<char> a;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        char ch = s[i];
+
+        // condition one - if opening bracket , push into stack
+        // condition two - if closing bracket , match and pop
+
+        if (ch == '(' || ch == '{' || ch == '[') {
+            a.push(ch);
+        }
+
+        else {
+
+            // for closing bracket
+
+            if (!a.empty()) {
+                char top = a.top();
+
+                if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) {
+                    a.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+
+            else {
+                return false;
+            }
+        }
+    }
+
+  if(a.empty())
+    {
+        cout << "Balanced" << endl;
+        return true;
+    }
+
+    else 
+    {
+        cout << "Not Balanced" << endl;
+        return false;  
+    }
+  
+  
+
+ return 0;
+}
