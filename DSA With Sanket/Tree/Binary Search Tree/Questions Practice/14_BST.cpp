4ebd37c63c950e74e9d936fb83104191e1e1d78f
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-08-22 22:28:05 +0530

    Refactor error handling in 19_Student_Page.cpp: enhance validation logic and improve user experience with clearer and more informative error messages for invalid student entries.

---
+++ DSA With Sanket/Tree/Binary Search Tree/Questions Practice/14_BST.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        
+        Node(int data) {
+            this -> data = data;
+            this -> left = NULL;
+            this -> right = NULL;
+        }
+
+        void addNode(int data) {
+
+            if(data < this -> data) {
+                
+                if(this -> left == NULL) {
+                    this -> left = new Node(data);
+                }
+                else {
+                    this -> left -> addNode(data);
+                }
+            }
+            else {
+                if(this -> right == NULL) {
+                    this -> right = new Node(data);
+                }
+                else {
+                    this -> right -> addNode(data);
+                }
+            }
+        }
+
+        
+
+};
+
+void printTree(Node *root) {
+
+    if(root == NULL) {
+        return;
+    }
+
+    cout << root -> data << " ";
+    printTree(root -> left);
+    printTree(root -> right);
+}
+
+int main() {
+
+    Node *root = new Node(12);
+    root -> addNode(10);
+    root -> addNode(15);
+    root -> addNode(11);
+    root -> addNode(13);
+    root -> addNode(14);
+    root -> addNode(16);
+    root -> addNode(17);
+
+    printTree(root);
+
+
+ return 0;
+}
